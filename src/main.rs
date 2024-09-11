cc43a4a146ba6e8d02136a72cfa7d538a4899e66
cutsea110 <cutsea110@gmail.com>
2024-09-11 12:01:33 +0900

    define employee

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+use chrono::NaiveDate;
+use dyn_clone::DynClone;
+use std::fmt::Debug;
+use std::ops::RangeInclusive;
+
+type EmployeeId = u32;
+
+#[derive(Debug, Clone, PartialEq)]
+struct TimeCard {
+    date: NaiveDate,
+    hours: f32,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct SalesReceipt {
+    date: NaiveDate,
+    amount: f32,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct Paycheck {
+    period: RangeInclusive<NaiveDate>,
+    gross_pay: f32,
+    deductions: f32,
+    net_pay: f32,
+}
+
+trait PaymentClassification: DynClone + Debug {
+    fn calculate_pay(&self, pc: &Paycheck) -> f32;
+}
+dyn_clone::clone_trait_object!(PaymentClassification);
+
+trait PaymentSchedule: DynClone + Debug {
+    fn is_pay_date(&self, date: NaiveDate) -> bool;
+    fn calculate_period(&self, payday: NaiveDate) -> RangeInclusive<NaiveDate>;
+}
+dyn_clone::clone_trait_object!(PaymentSchedule);
+
+trait PaymentMethod: DynClone + Debug {
+    fn pay(&self, pc: &Paycheck);
+}
+dyn_clone::clone_trait_object!(PaymentMethod);
+
+trait Affiliation: DynClone + Debug {
+    fn calculate_deductions(&self, pc: &Paycheck) -> f32;
+}
+
+#[derive(Debug, Clone)]
+struct Employee {
+    emp_id: EmployeeId,
+    name: String,
+    address: String,
+
+    classification: Box<dyn PaymentClassification>,
+    schedule: Box<dyn PaymentSchedule>,
+    method: Box<dyn PaymentMethod>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
