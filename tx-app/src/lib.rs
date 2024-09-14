345cf606c5bbef8b44c502fe929495d5f35f15b1
cutsea110 <cutsea110@gmail.com>
2024-09-14 21:37:19 +0900

    arrange tx-app module structure

--- tx-app/src/lib.rs
+++ tx-app/src/lib.rs
@@ -1,20 +1,7 @@
-use abstract_tx::UsecaseError;
+mod transaction;
+mod transaction_application;
+mod transaction_source;
 
-pub trait Transaction<Ctx> {
-    fn execute(&self, ctx: &mut Ctx) -> Result<(), UsecaseError>;
-}
-
-pub trait TransactionSource<Ctx> {
-    fn get_transaction(&mut self) -> Option<Box<dyn Transaction<Ctx>>>;
-}
-
-pub trait TransactionApplication<Ctx> {
-    fn tx_source(&self) -> impl TransactionSource<Ctx>;
-    fn run(&mut self, ctx: &mut Ctx) -> Result<(), UsecaseError> {
-        let mut tx_source = self.tx_source();
-        while let Some(tx) = tx_source.get_transaction() {
-            let _ = tx.execute(ctx);
-        }
-        Ok(())
-    }
-}
+pub use transaction::Transaction;
+pub use transaction_application::TransactionApplication;
+pub use transaction_source::TransactionSource;
---
+++ tx-app/src/transaction_application.rs
@@ -0,0 +1,14 @@
+use abstract_tx::UsecaseError;
+
+use crate::transaction_source::TransactionSource;
+
+pub trait TransactionApplication<Ctx> {
+    fn tx_source(&self) -> impl TransactionSource<Ctx>;
+    fn run(&mut self, ctx: &mut Ctx) -> Result<(), UsecaseError> {
+        let mut tx_source = self.tx_source();
+        while let Some(tx) = tx_source.get_transaction() {
+            let _ = tx.execute(ctx);
+        }
+        Ok(())
+    }
+}
